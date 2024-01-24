b18c550e305334300b604f681727e36fef55f89a
aayushchhetri2 <aayush.b.lamichhane@gmail.com>
2024-01-24 15:31:22 +0545

    updated

--- todo list using react/todolist/src/App.js
+++ todo list using react/todolist/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import ToDoLIst from './ToDoLIst';
 
 function App() {
   return (
     <>
+    <ToDoLIst/>
     </>
   );
 }
---
+++ todo list using react/todolist/src/ToDoLIst.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ToDoLIst = () => {
+  return (
+    <div>ToDoLIst</div>
+  )
+}
+
+export default ToDoLIst
