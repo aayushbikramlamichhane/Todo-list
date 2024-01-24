d025d93fe6519c703a9121d683ed4e3ac9334d65
aayushchhetri2 <aayush.b.lamichhane@gmail.com>
2024-01-24 15:44:02 +0545

    next

--- todo list using react/todolist/src/ToDoLIst.js
+++ todo list using react/todolist/src/ToDoLIst.js
@@ -5,7 +5,7 @@ const ToDoLIst = () => {
     const [newTask,setnewTasks] = useState("");
 
     function handleInputChange(event){
-
+        setnewTasks(event.target.value);
     }
     function addTask(){
 
@@ -17,12 +17,20 @@ const ToDoLIst = () => {
 
     }
     function moveTaskDown(index){
-        
+
     }
 
 
   return (
-  )
+    <div className='to-do-list'>
+        <h1>Todo List</h1>
+        <div>
+            <input type='text'placeholder='Enter the task...' 
+            value={newTask}
+            onChange={handleInputChange}/>
+        </div>
+    </div>
+  );
 }
 
 export default ToDoLIst
