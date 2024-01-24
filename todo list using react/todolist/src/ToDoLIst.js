fc4d49b6ee894bd168d4a05bbc5c1545848e0509
aayushchhetri2 <aayush.b.lamichhane@gmail.com>
2024-01-24 15:37:03 +0545

    commit

--- todo list using react/todolist/src/ToDoLIst.js
+++ todo list using react/todolist/src/ToDoLIst.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ToDoLIst = () => {
+    const [tasks,settasks] = useState([]);
+    const [newTask,setnewTasks] = useState("");
+
+    function handleInputChange(event){
+
+    }
+    function addTask(){
+
+    }
+    function deleteTask(index){
+
+    }
+    function moveTaskUp(index){
+
+    }
+    function moveTaskDown(index){
+        
+    }
+
+
   return (
-    <div>ToDoLIst</div>
   )
 }
 
