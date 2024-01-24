19ce1fa5e22c21e29cbcac3cb95e4a478472c2cc
aayushchhetri2 <aayush.b.lamichhane@gmail.com>
2024-01-24 15:59:39 +0545

    css

--- todo list using react/todolist/src/ToDoLIst.js
+++ todo list using react/todolist/src/ToDoLIst.js
@@ -29,14 +29,22 @@ const ToDoLIst = () => {
             value={newTask}
             onChange={handleInputChange}/>
             <button className='add-button' onClick={addTask}>
-                Add
+                Add👍
                 </button>
         </div>
         <ol>
             {tasks.map((task,index) =>
             <li key={index}>
                 <span className='text'>{task}</span>
-                <button>Delete</button>
+                <button className='delete-button' onClick={() => deleteTask(index)}>
+                    Delete😒
+                </button>
+                <button className='move-button' onClick={() => moveTaskUp(index)}>
+                ☝️
+                </button>   
+                <button className='move-button' onClick={() => moveTaskDown(index)}>
+                👇
+                </button>  
             </li>
             )}
         </ol>
